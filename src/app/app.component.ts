48d9d3004e74e5f56697b3323d700929f05e3f9a
Jeffrey Buehler <jbuehler@apolloei.com>
2020-05-08 07:11:10 -0600

    bad lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gh-actions- test';
-}
+   title = 'gh-actions- test'; 
+} 
+ 
